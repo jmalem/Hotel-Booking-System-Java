4375de8064110a56a4f6b3724c50dc7b9ffac9d8
jansen <z5141970@student.unsw.edu.au>
2018-08-13 14:26:47 +1000

    create hotel and add rooms succeeded, but rooms has not get into the rooms system, must work on it next

--- HotelSystem/src/ass1/Hotel.java
+++ HotelSystem/src/ass1/Hotel.java
@@ -3,25 +3,27 @@ package ass1;
 import java.util.ArrayList;
 
 public class Hotel {
+	private String name;	
 	private int numRooms;
 	RoomSystem roomSys;
 	//BookingSystem bookSys;
 	private ArrayList<String> users;
 	
-	public Hotel(ArrayList<Room> rooms) {
-		this.numRooms = numRooms;
+	public Hotel(String name, ArrayList<Room> rooms) {
+		this.numRooms = rooms.size();
+		this.name = name;
 		this.roomSys = new RoomSystem();
+		
 		this.users = new ArrayList<String>();
+	}
+	public String getHotelName() {
+		return this.name;
 	}
 
 	public int getCapacity() {
 		return numRooms;
 	}
-
-	public void setCapacity(int num) {
-		this.numRooms = num;
-	}
-
+	
 	public ArrayList<String> getUsers() {
 		return users;
 	}
@@ -29,5 +31,6 @@ public class Hotel {
 	public void addUsers(String user) {
 		this.users.add(user);
 	}
+	
 	
 }
